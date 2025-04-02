e9adfbb1c63966673ba03f0b44e063023172b9de
HsinTing Wang <hsintingwang@HsinTingdeMacBook-Air.local>
2025-04-02 12:13:46 -0700

    update front page color

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ function buttonClicked() {
     document.getElementById("mainDiv").textContent =
         "Button Clicked times: " + numButtonClicks;
 }
+
+let lastScrollY = window.scrollY;
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > lastScrollY) {
+    // 向下滾動，隱藏導航列
+    nav.style.transform = "translateY(-100%)";
+  } else {
+    // 向上滾動，顯示導航列
+    nav.style.transform = "translateY(0)";
+  }
+  lastScrollY = window.scrollY;
+});
